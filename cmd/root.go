05b7ee952054a3a1ecdbb15c1e89c0d8d1ddf83b
Alex Zero <joseph@marsden.space>
2022-05-07 20:29:14 +0100

    fix: add jpath option

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"log"
+	"os"
 
 	jsonnet "github.com/google/go-jsonnet"
 	"github.com/spf13/cobra"
@@ -11,6 +11,8 @@ import (
 )
 
 var options struct {
+	outputFile   string
+	jPathDirs    []string
 	extCodeFiles map[string]string
 }
 
@@ -28,10 +30,15 @@ var rootCmd = &cobra.Command{
 			vm.NativeFunction(nf)
 		}
 
+		vm.Importer(&jsonnet.FileImporter{
+			JPaths: options.jPathDirs,
+		})
+
 		for k, v := range options.extCodeFiles {
 			contents, _, err := vm.ImportAST(v, v)
 			if err != nil {
-				log.Fatal(err)
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
 			}
 
 			vm.ExtNode(k, contents)
@@ -39,10 +46,15 @@ var rootCmd = &cobra.Command{
 
 		result, err := vm.EvaluateFile(file)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
 		}
 
-		fmt.Print(result)
+		err = WriteOutputFile(result, options.outputFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 	},
 }
 
@@ -53,5 +65,29 @@ func Execute() {
 }
 
 func init() {
+	rootCmd.PersistentFlags().StringVarP(&options.outputFile, "output-file", "o", "", "File to be written to")
+	rootCmd.PersistentFlags().StringArrayVarP(&options.jPathDirs, "jpath", "J", []string{}, "Specify an additional library search dir (right-most wins)")
 	rootCmd.PersistentFlags().StringToStringVar(&options.extCodeFiles, "ext-code-file", map[string]string{}, "Read the code from the file")
+
+	rootCmd.MarkPersistentFlagRequired("output-file")
+}
+
+func WriteOutputFile(output string, outputFile string) (err error) {
+	if outputFile == "" {
+		fmt.Print(output)
+		return nil
+	}
+
+	f, createErr := os.Create(outputFile)
+	if createErr != nil {
+		return createErr
+	}
+	defer func() {
+		if ferr := f.Close(); ferr != nil {
+			err = ferr
+		}
+	}()
+
+	_, err = f.WriteString(output)
+	return err
 }
